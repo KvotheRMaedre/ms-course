21ac7c27a5d520935b35f85c925e6a4373fc31c9
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-17 14:20:38 -0300

    hr-config-server: adding the spring-cloud-starter-config and getting config from the hr-config-server

--- hr-worker/src/main/java/tech/kvothe/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/tech/kvothe/hrworker/controller/WorkerController.java
@@ -2,6 +2,7 @@ package tech.kvothe.hrworker.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,9 @@ import java.util.List;
 @RequestMapping("worker")
 public class WorkerController {
 
+    @Value("${test.config}")
+    private String testConfig;
+
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 
     private final Environment env;
@@ -26,6 +30,14 @@ public class WorkerController {
     public WorkerController(Environment env, WorkerService workerService) {
         this.env = env;
         this.workerService = workerService;
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<Void> findConfig() {
+        logger.info("CONFIG = " + testConfig);
+        var worker = workerService.findAll();
+
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping()
