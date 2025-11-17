af6cf783e4c159b678f1bddaeb3b909027ca8603
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-17 13:03:45 -0300

    hr-config-server: creating the project

---
+++ hr-config-server/src/test/java/tech/kvothe/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
