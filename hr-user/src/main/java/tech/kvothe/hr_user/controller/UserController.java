15bab8a5c0f552be18bc2bb932f695c1849d0d9b
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-17 17:39:51 -0300

    hr-user: creating routes to find user by id and email

---
+++ hr-user/src/main/java/tech/kvothe/hr_user/controller/UserController.java
@@ -0,0 +1,35 @@
+package tech.kvothe.hr_user.controller;
+
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tech.kvothe.hr_user.entity.User;
+import tech.kvothe.hr_user.service.UserService;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/email/{email}")
+    public ResponseEntity<User> findUserByEmail(@PathVariable("email") String email) {
+        var user = userService.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findUserByEmail(@PathVariable("id") Long id) {
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+
+}
---
+++ hr-user/src/main/java/tech/kvothe/hr_user/service/UserService.java
@@ -0,0 +1,25 @@
+package tech.kvothe.hr_user.service;
+
+import org.springframework.stereotype.Service;
+import tech.kvothe.hr_user.entity.User;
+import tech.kvothe.hr_user.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
