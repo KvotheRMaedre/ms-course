e60cbe8f7e7a3597d979066465f2bcba0c447233
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-17 14:29:05 -0300

    hr-worker: adding an actuator to refresh config without needing to restart the application

--- hr-worker/src/main/java/tech/kvothe/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/tech/kvothe/hrworker/controller/WorkerController.java
@@ -3,6 +3,7 @@ package tech.kvothe.hrworker.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("worker")
+@RefreshScope
 public class WorkerController {
 
     @Value("${test.config}")
