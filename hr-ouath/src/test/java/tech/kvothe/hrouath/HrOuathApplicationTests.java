3d640ca7a6fbcb87ab1ff4d86718b7e0eaf77b48
Ítalo H. <italodssouza.backup@gmail.com>
2025-11-19 09:40:50 -0300

    hr-oauth: creating project

---
+++ hr-ouath/src/test/java/tech/kvothe/hrouath/HrOuathApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.hrouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOuathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
